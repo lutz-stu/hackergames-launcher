a2b72d9d2dff552626ad3a083b863084b0eece97
Lutz <lutz.stuckert@posteo.de>
2024-11-02 00:11:35 +0100

    i give up at installing at within the build and doing it online now

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ function createWindow() {
         width: 1280,
         height: 750,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true,
+            preload: path.join(app.getAppPath(), 'preload.js'),
+            nodeIntegration: false,
             contextIsolation: true,
-            additionalArguments: [`--appPath=${app.getAppPath()}`]
+            additionalArguments: [`--appPath=${app.getAppPath()}`],
+            enableRemoteModule: false
         }
     });
 
@@ -20,3 +21,5 @@ app.on('ready', createWindow);
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
+
+app.commandLine.appendSwitch('disable-gpu');
--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 
 function launchGame(gamePath) {
     const fullPath = window.electronAPI.getGamePath(gamePath);
-    exec(`"${fullPath}"`, (error, stdout, stderr) => {
+    exec("${fullPath}", (error, stdout, stderr) => {
         if (error) {
             console.error(`Error starting game: ${error}`);
             return;
